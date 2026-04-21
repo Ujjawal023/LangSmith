487848b9f73c4e2f2906c4864bc2b1ffa1d226bc
Ujjawal023 <ujjawalchoudharyy@gmail.com>
2026-04-22 05:25:20 +0530

    first commit

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -1,14 +1,15 @@
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 # Simple one-line prompt
 prompt = PromptTemplate.from_template("{question}")
 
-model = ChatOpenAI()
+model = ChatGroq(model='llama-3.1-8b-instant')
 parser = StrOutputParser()
 
 # Chain: prompt → model → parser
@@ -17,3 +18,4 @@ chain = prompt | model | parser
 # Run it
 result = chain.invoke({"question": "What is the capital of Peru?"})
 print(result)
+
